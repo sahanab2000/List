30ccd266c5c88708072fe774787100ab8df4b94e
sahanab2000 <85185990+sahanab2000@users.noreply.github.com>
2022-11-13 12:50:01 +0530

    Add files via upload

---
+++ list.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C# Collections");
+            doListDemo();
+        }
+        private static void doListDemo()
+        {
+            Console.WriteLine("\nIn doListDeno");
+            //Creating List
+            List<string> list = new List<string>(); //Adding object to the list
+            list.Add("Arya");
+            list.Add("Rushank");
+            list.Add("Sparsha");
+            list.Add("Samagna");
+
+            // Iterating list elements
+            foreach (var element in list)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
+
